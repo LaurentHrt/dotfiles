d6108b93e3a2c7a4df7fb506811d8ee5358a7a2e
Laurent Hirth <laurent.hirth.68@icloud.com>
2023-08-24 11:26:04 +0200

    Update .chezmoiignore Add .config/nvim/LICENSE.md Add .config/nvim/README.md Add .config/nvim/doc/kickstart.txt Add .config/nvim/doc/tags Add .config/nvim/.git/COMMIT_EDITMSG Add .config/nvim/.git/FETCH_HEAD Add .config/nvim/.git/HEAD Add .config/nvim/.git/config Add .config/nvim/.git/description Add .config/nvim/.git/hooks/applypatch-msg.sample Add .config/nvim/.git/hooks/commit-msg.sample Add .config/nvim/.git/hooks/fsmonitor-watchman.sample Add .config/nvim/.git/hooks/post-update.sample Add .config/nvim/.git/hooks/pre-applypatch.sample Add .config/nvim/.git/hooks/pre-commit.sample Add .config/nvim/.git/hooks/pre-merge-commit.sample Add .config/nvim/.git/hooks/pre-push.sample Add .config/nvim/.git/hooks/pre-rebase.sample Add .config/nvim/.git/hooks/pre-receive.sample Add .config/nvim/.git/hooks/prepare-commit-msg.sample Add .config/nvim/.git/hooks/push-to-checkout.sample Add .config/nvim/.git/hooks/sendemail-validate.sample Add .config/nvim/.git/hooks/update.sample Add .config/nvim/.git/index Add .config/nvim/.git/info/exclude Add .config/nvim/.git/logs/HEAD Add .config/nvim/.git/logs/refs/heads/master Add .config/nvim/.git/logs/refs/remotes/origin/HEAD Add .config/nvim/.git/logs/refs/remotes/origin/master Add .config/nvim/.git/objects/0f/1cf42278d158ac2c117fd7317854c5528dc23f Add .config/nvim/.git/objects/39/667028bac3e35302f258410c443cb355f069b1 Add .config/nvim/.git/objects/62/307f0cbfe4095d81436adcf07b6860d0e89fa8 Add .config/nvim/.git/objects/85/dc2e78b6c25eabc04ad8c68bbe9cc093a4e259 Add .config/nvim/.git/objects/ba/265c4e8c105442aacd981860e106bc52bbebf4 Add .config/nvim/.git/objects/d1/7b1babddbf33a6708f5df6d5de828982021c62 Add .config/nvim/.git/objects/fd/470adf0c1414874d9ac5dfa87729f92d29d710 Add .config/nvim/.git/objects/info/.keep Add .config/nvim/.git/objects/pack/pack-24b6426cc93ef0dd8be436859fa1ecc2d66a04d3.idx Add .config/nvim/.git/objects/pack/pack-24b6426cc93ef0dd8be436859fa1ecc2d66a04d3.pack Add .config/nvim/.git/objects/pack/pack-24b6426cc93ef0dd8be436859fa1ecc2d66a04d3.rev Add .config/nvim/.git/packed-refs Add .config/nvim/.git/refs/heads/master Add .config/nvim/.git/refs/remotes/origin/HEAD Add .config/nvim/.git/refs/remotes/origin/master Add .config/nvim/.git/refs/tags/.keep Add .config/nvim/.github/ISSUE_TEMPLATE/bug_report.md Add .config/nvim/.gitignore Add .config/nvim/.stylua.toml Add .config/nvim/init.lua Add .config/nvim/lua/custom/keymaps/init.lua Add .config/nvim/lua/custom/plugins/init.lua Add .config/nvim/lua/kickstart/plugins/autoformat.lua Add .config/nvim/lua/kickstart/plugins/debug.lua

---
+++ dot_config/nvim/lua/custom/keymaps/init.lua
@@ -0,0 +1,7 @@
+-- Move to window using the <ctrl> hjkl keys
+vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Go to left window", remap = true })
+vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = true })
+vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", noremap = true })
+vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
+
+return {}
