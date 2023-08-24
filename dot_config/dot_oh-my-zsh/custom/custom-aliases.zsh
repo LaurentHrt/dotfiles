2c81a5d80d6fe038e49607421c750dfe84f5c30b
Laurent Hirth <laurent.hirth.68@icloud.com>
2023-08-24 11:21:41 +0200

    Add .cbtrc Add .config/Brewfile Add .config/atuin/config.toml Add .config/.ideavimrc Add .config/.oh-my-zsh/custom/custom-aliases.zsh Add .config/.oh-my-zsh/custom/sidel-aliases.zsh Add .config/gcloud/configurations/config_cloud-code Add .config/gcloud/configurations/config_komportementalist-staging Add .config/gcloud/configurations/config_sidel-data Add .config/gcloud/configurations/config_sidel-deve-back Add .config/gcloud/configurations/config_sidel-deve-front Add .config/gcloud/configurations/config_sidel-prod-back Add .config/gcloud/configurations/config_sidel-prod-front Add .config/gcloud/configurations/config_default Add .config/gh/config.yml Add .config/gh/hosts.yml Add .config/git/config Add .config/git/config-ouidou Add .config/git/config-personal Add .config/git/config-sidel Add .config/gita/groups.csv Add .config/gita/repos.csv Add .config/karabiner/assets/complex_modifications/1692592270.json Add .config/karabiner/karabiner.json Add .config/starship.toml Add .config/tmux/tmux.conf Add .config/topgrade.toml Add .config/wezterm/wezterm.lua Add .myclirc Add .vimrc Add .zprofile Add .zshrc Add .hushlogin

---
+++ dot_config/dot_oh-my-zsh/custom/custom-aliases.zsh
@@ -0,0 +1,56 @@
+alias sudo='sudo '
+# alias h="history"
+alias ws="webstorm"
+alias edit="open -e"
+alias cat='bat'
+alias als='alias | grep'
+alias cd='z'
+alias omzr='omz reload'
+alias aliasedit='nvim ~/.config/.oh-my-zsh/custom/custom-aliases.zsh'
+alias editalias='aliasedit'
+alias zshconfig='nvim ~/.zshrc'
+# alias gita='python3 -m gita'
+alias j='jobs'
+alias pm='pm2'
+alias pml='pm2 list --sort id'
+alias pmlg='pm2 logs'
+alias pmevodev='pm2 start ~/workspace/sidel/evoon-pm.config.js'
+alias pmsa='pm2 stop all'
+alias sshraspi='ssh laurent@192.168.10.21 -p 22001 -i ~/.ssh/id_ed25519.raspi'
+alias raspissh='sshraspi'
+alias sshraspiw='ssh laurent@jupiter3.duckdns.org -p 22001 -i ~/.ssh/id_ed25519.raspi'
+alias exa="exa --icons --group-directories-first -hg --git"
+alias ls='exa'
+alias la='exa -a'
+alias lla='exa -al'
+alias l='exa -l'
+alias lt='exa -T --git-ignore --level=2 --group-directories-first'
+alias llt='exa -lT --git-ignore --level=2 --group-directories-first'
+alias llat='exa -alT --git-ignore --level=2 --group-directories-first'
+alias lT='exa -T --git-ignore --level=4 --group-directories-first'
+# alias backup-dot-files='(cd ~/workspace/personal/my-dotfiles && ./backup.sh)'
+# alias vim=nvim
+alias vi=nvim
+alias pdm=podman
+alias pdmms='podman machine stop'
+alias rip='rip --graveyard ~/.Trash/'
+alias wrapon='tput rmam'
+alias wrapoff='tput smam'
+# alias rm='rm -i'
+alias npr='npm run'
+alias nprd='nvm use; npm run dev'
+alias nvmu='nvm use'
+alias d="cd"
+
+# Create a Brewfile file backup
+alias brewBackup="(cd ~/.config && rm Brewfile && brew bundle dump)"
+ 
+# Helm
+alias h="helm"
+alias hin="helm install"
+alias hse="helm search"
+alias hup="helm upgrade"
+alias hun="helm uninstall"
+
+# fnm nvm
+alias nvm="fnm"
---
+++ dot_config/dot_oh-my-zsh/custom/sidel-aliases.zsh
@@ -0,0 +1,151 @@
+alias editsidelalias='vim ~/.config/.oh-my-zsh/custom/sidel-aliases.zsh'
+
+# Connections
+alias sshvmlaval='ssh administrator@172.26.223.82 -i ~/.ssh/id_rsa.vmlaval'
+
+# Database
+alias docker=podman
+alias stoppdm='podman machine stop'
+alias startsql='pdmmachinestart ; podman container start mysql-onprem'
+alias startinfluxdb='pdmmachinestart ; podman container start influxdb'
+alias createsql='(\cd ~/workspace/sidel/sidel-sql && docker run -d --name mysql-onprem -e MYSQL_ROOT_PASSWORD=root -p 3366:3306 --network sidel-sql --network-alias mysql biarms/mysql:5.7 --sql_mode="ONLY_FULL_GROUP_BY" --default-time-zone="+00:00")'
+alias run-sql-migration='(\cd ~/workspace/sidel/sidel-sql && docker start mysql-onprem && docker build -t sidel-migration-tool -f migration_tool/build/Dockerfile-macos . && docker run --rm -it -e MYSQL_USER=root -e MYSQL_PASSWORD=root -e MYSQL_HOST=mysql -e MYSQL_PORT=3306 -e API_BACK_PASSWORD=api_back -e DTM_LIVE_LOCAL_PASSWORD=dtm_live_local --name sidel-migration-tool --network sidel-sql sidel-migration-tool)'
+alias buildsql='(\cd ~/workspace/sidel/sidel-sql && docker build -t sidel-migration-tool -f migration_tool/build/Dockerfile-macos .)'
+alias connectsql='mycli -u root -h localhost -P 3366 -p root'
+alias connectsqleit='mycli -u root -h localhost -P 3366 eit_v7 -p root'
+alias checklocalmigration='mycli -u root -h localhost -P 3366 eit_v7 -p root -e "select * from _migrations order by id desc limit 5" | column -t'
+alias checkdevmigration='mycli -u API_BACK -h localhost -P 3307 SIDEL_IOT_DEV -p dBycsDKwmg8hCA9q2ZT4NUeP7MD5wn2V -e "select * from _migrations order by id desc limit 5" | column -t'
+
+# gita
+alias pullall='gita super sidel-all pull'
+alias swdall='gita super sidel-all switch developpement'
+alias pullfb='gita super sidel-fb pull'
+alias gswfb='gita super sidel-fb switch'
+alias gswfbd='gswfb developpement && pullfb'
+alias resetsideldev='swdall && pullall'
+alias resetsideldevhard='gita super sidel-all restore . && swdall && pullall'
+
+# podman machine
+pdmmachinestart() {
+	# Check if podman machine is already running
+	machineState=$(pdm machine info --format=json | jq '.Host.MachineState')
+	if [[ $machineState = '"Running"' ]] then
+		echo "Podman machine already running"
+	else 
+    podman machine start
+	fi
+}
+
+# env management
+setcloudenv() {
+	echo "Setting cloud env..."
+	cp ~/workspace/sidel/sidel-back/.env.dev ~/workspace/sidel/sidel-back/.env
+	cp ~/workspace/sidel/sidel-front/src/environments/environment.env.ts ~/workspace/sidel/sidel-front/src/environments/environment.ts 
+	cp ~/workspace/sidel/sidel-recovery-machine/.env.dev ~/workspace/sidel/sidel-recovery-machine/.env 
+	echo "Cloud env set"
+}
+
+setonpremenv() {
+	echo "Setting on-premise env..."
+	cp ~/workspace/sidel/sidel-back/.env.onpremise ~/workspace/sidel/sidel-back/.env
+	cp ~/workspace/sidel/sidel-front/src/environments/environment.opremise.ts ~/workspace/sidel/sidel-front/src/environments/environment.ts 
+	cp ~/workspace/sidel/sidel-recovery-machine/.env.on-premise ~/workspace/sidel/sidel-recovery-machine/.env 
+	echo "On-premise env set"
+}
+
+# Process management
+alias openprosys='open -a Prosys\ OPC\ UA\ Simulation\ Server'
+alias closeprosys='osascript -e "quit app \"Prosys OPC UA Simulation Server\"" &'
+
+alias startevocloud="setcloudenv ; stopdtm ; stoprecovery ; stopscheduler ; startproxydev ; startsidelback ; startsidelfront"
+alias startdtm="startdtmback ; startdtmfront ; openprosys"
+alias startevoonprem="setonpremenv ; stopproxydev ; startsql ; startsidelback ; startsidelfront"
+alias startfullevoonprem="openprosys ; startevoonprem ; startinfluxdb ; startdtm ; startscheduler ; startrecovery"
+
+alias stopevo='pm2 stop sidel-front sidel-back proxy-dev > /dev/null'
+alias stopdtm='pm2 stop dtm-back dtm-front > /dev/null ; closeprosys'
+alias stopproxydev='pm2 stop proxy-dev > /dev/null'
+alias stopproxyprod='pm2 stop proxy-prod > /dev/null'
+alias stopall='pm2 stop all > /dev/null ; closeprosys'
+alias stoprecovery='pm2 stop sidel-recovery-machine > /dev/null'
+alias stopscheduler='pm2 stop task-scheduler > /dev/null'
+
+startsidelfront() {
+	if [[ -z $(pm2 pid sidel-front) ]] then
+		echo "start sidel-front"
+    (\cd ~/workspace/sidel/sidel-front && nvm use > /dev/null && pm2 start "npm run start" --name "sidel-front" --error "~/.pm2/logs/sidel-front.log"  --output "~/.pm2/logs/sidel-front.log" > /dev/null)
+	else
+		echo "restart sidel-front"
+    pm2 restart sidel-front > /dev/null
+	fi
+}
+
+startsidelback() {
+	if [[ -z $(pm2 pid sidel-back) ]] then
+		echo "start sidel-back"
+    (\cd ~/workspace/sidel/sidel-back && nvm use > /dev/null && pm2 start "npm run start:dev" --name "sidel-back" --error "~/.pm2/logs/sidel-back.log" --output "~/.pm2/logs/sidel-back.log" > /dev/null)
+	else
+		echo "restart sidel-back"
+    pm2 restart sidel-back > /dev/null
+	fi
+}
+
+startproxydev() {
+	if [[ -z $(pm2 pid proxy-dev) ]] then
+		echo "start proxy-dev"
+    (\cd ~/workspace/sidel/sidel-back && nvm use > /dev/null && pm2 start "npm run proxy:dev-file" --name "proxy-dev" --error "~/.pm2/logs/proxy-dev.log" --output "~/.pm2/logs/proxy-dev.log" > /dev/null)
+	else
+		echo "restart proxy-dev"
+    pm2 restart proxy-dev > /dev/null
+	fi
+}
+
+startproxyprod() {
+	if [[ -z $(pm2 pid proxy-prod) ]] then
+		echo "start proxy-prod"
+    (\cd ~/workspace/sidel/sidel-back && nvm use > /dev/null && pm2 start "npm run proxy:prod-file" --name "proxy-prod" --error "~/.pm2/logs/proxy-prod.log" --output "~/.pm2/logs/proxy-prod.log" > /dev/null)
+	else
+		echo "restart proxy-prod"
+    pm2 restart proxy-prod > /dev/null
+	fi
+}
+
+startdtmfront() {
+	if [[ -z $(pm2 pid dtm-front) ]] then
+		echo "start dtm-front"
+    (\cd ~/workspace/sidel/dtm-live-front && nvm use 16 > /dev/null && pm2 start "npm run start" --name "dtm-front" --error "~/.pm2/logs/dtm-front.log" --output "~/.pm2/logs/dtm-front.log" > /dev/null)
+	else
+		echo "restart dtm-front"
+    pm2 restart dtm-front > /dev/null
+	fi
+}
+
+startdtmback() {
+	if [[ -z $(pm2 pid dtm-back) ]] then
+		echo "start dtm-back"
+    (\cd ~/workspace/sidel/dtm-live && nvm use 16 > /dev/null && pm2 start "npm run start:dev" --name "dtm-back" --error "~/.pm2/logs/dtm-back.log" --output "~/.pm2/logs/dtm-back.log" > /dev/null)
+	else
+		echo "restart dtm-back"
+    pm2 restart dtm-back > /dev/null
+	fi
+}
+
+startscheduler() {
+	if [[ -z $(pm2 pid task-scheduler) ]] then
+		echo "start task-scheduler"
+    (\cd ~/workspace/sidel/task-scheduler && nvm use 16 > /dev/null && pm2 start "npm run start:dev" --name "task-scheduler" --error "~/.pm2/logs/task-scheduler.log" --output "~/.pm2/logs/task-scheduler.log" > /dev/null)
+	else
+		echo "restart task-scheduler"
+    pm2 restart task-scheduler > /dev/null
+	fi
+}
+
+startrecovery() {
+	if [[ -z $(pm2 pid sidel-recovery-machine) ]] then
+		echo "start sidel-recovery-machine"
+    (\cd ~/workspace/sidel/sidel-recovery-machine && nvm use 16 > /dev/null && pm2 start "npm run start:dev" --name "sidel-recovery-machine" --error "~/.pm2/logs/sidel-recovery-machine.log" --output "~/.pm2/logs/sidel-recovery-machine.log" > /dev/null)
+	else
+		echo "restart sidel-recovery-machine"
+    pm2 restart sidel-recovery-machine > /dev/null
+	fi
+}
