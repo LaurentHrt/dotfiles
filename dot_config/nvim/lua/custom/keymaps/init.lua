7e359debd22c90f7627a04227e4648c6dc059f3b
Laurent Hirth <laurent.hirth.68@icloud.com>
2023-09-22 19:16:21 +0200

    Update .config/Brewfile Update .config/.oh-my-zsh/custom/custom-aliases.zsh Update .config/.oh-my-zsh/custom/sidel-aliases.zsh Update .config/nvim/lua/custom/keymaps/init.lua Update .config/nvim/lua/kickstart/plugins/debug.lua Update .config/starship.toml Update .config/tmux/tmux.conf Update .myclirc Update .zshrc

--- dot_config/nvim/lua/custom/keymaps/init.lua
+++ dot_config/nvim/lua/custom/keymaps/init.lua
@@ -4,8 +4,22 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Go to lower window", remap = tr
 vim.keymap.set("n", "<C-k>", "<C-w>k", { desc = "Go to upper window", noremap = true })
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Go to right window", remap = true })
 
+-- Neotree
 vim.keymap.set("n", "<leader>e",
 	function() require("neo-tree.command").execute({ toggle = true }) end,
 	{ desc = "Toggle NeoTree", noremap = true })
+
+-- Buffers
+vim.keymap.set('n', '<leader>bn', '<cmd>bnext<cr>', { desc = '[N]ext [B]uffer' })
+vim.keymap.set('n', '<leader>bp', '<cmd>bprevious<cr>', { desc = '[P]revious [B]uffer' })
+
+-- Git
+vim.keymap.set('n', '<leader>gb', '<cmd>Git blame<cr>', { desc = '[G]it [B]lame' })
+
+-- Remove mapping of arrow keys while learning to use hjkl
+vim.keymap.set('n', '<Up>', '<Nop>')
+vim.keymap.set('n', '<Down>', '<Nop>')
+vim.keymap.set('n', '<Left>', '<Nop>')
+vim.keymap.set('n', '<Right>', '<Nop>')
 
 return {}
