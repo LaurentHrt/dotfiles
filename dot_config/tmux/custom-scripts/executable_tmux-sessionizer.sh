aff996bb0f5ee52654398a31139489743ed6f45d
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-01-31 17:48:13 +0100

    Add .chezmoiexternal.toml Update .config/git/config Update .config/git/config-komp Update .config/tmux/custom-scripts/tmux-sessionizer.sh Update install-packages.sh

--- dot_config/tmux/custom-scripts/executable_tmux-sessionizer.sh
+++ dot_config/tmux/custom-scripts/executable_tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/.config ~/workspace/personal ~/workspace ~/workspace/sidel ~/workspace/komportementalist -mindepth 1 -maxdepth 1 -type d | fzf)
+    selected=$(find ~/.config ~/workspace/personal ~/workspace ~/workspace/sidel ~/workspace/komp -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
