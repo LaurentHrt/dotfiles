0b3860ec358127f2319164754828ac5da05913bd
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-02-19 15:29:56 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,5 +1,15 @@
--- Pull in the wezterm API
 local wezterm = require("wezterm")
+local mux = wezterm.mux
+
+wezterm.on('gui-attached', function(domain)
+	-- maximize all displayed windows on startup
+	local workspace = mux.get_active_workspace()
+	for _, window in ipairs(mux.all_windows()) do
+		if window:get_workspace() == workspace then
+			window:gui_window():maximize()
+		end
+	end
+end)
 
 -- This table will hold the configuration.
 local config = {}
