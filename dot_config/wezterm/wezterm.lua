cbcd178b70d526b96391004dcb069e4a136c1f35
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-02-24 11:30:03 +0100

    add random background for wezterm

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -1,7 +1,7 @@
 local wezterm = require("wezterm")
 local mux = wezterm.mux
 
-wezterm.on('gui-attached', function(domain)
+wezterm.on('gui-attached', function(_)
 	-- maximize all displayed windows on startup
 	local workspace = mux.get_active_workspace()
 	for _, window in ipairs(mux.all_windows()) do
@@ -25,9 +25,19 @@ config.color_scheme = "tokyonight"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 
+-- Function to pick a random filename from a list
+local function getRandomFilename(files)
+	if #files == 0 then
+		return ''
+	end
+	local randomIndex = math.random(1, #files)
+	return files[randomIndex]
+end
+
+local randomFilename = getRandomFilename(wezterm.read_dir '/Users/laurent/.config/wezterm/wallpapers/')
 config.background = {
 	{
-		source = { File = '/Users/laurent/.config/wezterm/wallpaper.jpeg' },
+		source = { File = randomFilename },
 		hsb = {
 			brightness = 0.25,
 		},
