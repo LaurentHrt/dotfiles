e37dd59604f3875a2b5969270ffb72871ec76266
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-02-26 15:34:35 +0100

    Update .config/tmux/custom-scripts/tmux-sessionizer.sh

--- dot_config/tmux/custom-scripts/executable_tmux-sessionizer.sh
+++ dot_config/tmux/custom-scripts/executable_tmux-sessionizer.sh
@@ -3,7 +3,7 @@
 if [[ $# -eq 1 ]]; then
     selected=$1
 else
-    selected=$(find ~/.config ~/workspace -mindepth 1 -maxdepth 2 -type d | fzf)
+    selected=$(find ~/.config ~/workspace ~/workspace/* -mindepth 1 -maxdepth 1 -type d | fzf)
 fi
 
 if [[ -z $selected ]]; then
