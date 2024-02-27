aecca0e57e836a1e3fb83991eb3615a7cf192290
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-02-27 09:24:05 +0100

    Add .config/wezterm/utils.lua

---
+++ dot_config/wezterm/utils.lua
@@ -0,0 +1,26 @@
+local wezterm = require 'wezterm'
+
+local module = {}
+
+function module.filterImageFiles(filenames)
+	local filteredFiles = {}
+	for _, filename in ipairs(filenames) do
+		if filename:lower():find('%.jpg$') or filename:lower():find('%.jpeg$') or filename:lower():find('%.png$') or filename:lower():find('%.gif$') then
+			wezterm.log_error('match')
+			table.insert(filteredFiles, filename)
+		end
+	end
+
+	return filteredFiles
+end
+
+-- Function to pick a random filename from a list
+function module.getRandomElement(array)
+	if #array == 0 then
+		return nil
+	end
+	local randomIndex = math.random(1, #array)
+	return array[randomIndex]
+end
+
+return module
