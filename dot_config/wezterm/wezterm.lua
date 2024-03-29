9e29ab8c600b82b468ef285736b6f27dc8684619
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-03-29 17:25:29 +0100

    Update .config/wezterm/wezterm.lua

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -26,6 +26,8 @@ config.color_scheme = "tokyonight"
 config.enable_tab_bar = false
 config.window_decorations = "RESIZE"
 config.use_ime = true
+config.send_composed_key_when_left_alt_is_pressed = true
+config.send_composed_key_when_right_alt_is_pressed = true
 
 local imageFiles = utils.filterImageFiles(wezterm.read_dir(wezterm.home_dir .. "/.config/wezterm/wallpapers/"))
 local randomFilename = utils.getRandomElement(imageFiles)
