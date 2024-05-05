45d5296a2a4bd751a65d1fb6b939ee298a25cb96
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-05-05 17:39:05 +0200

    remove nnn

--- dot_config/nnn/executable_config.zsh
+++
@@ -1,5 +0,0 @@
-# set --export NNN_FIFO "/tmp/nnn.fifo"
-# BLK="0B" CHR="0B" DIR="04" EXE="06" REG="00" HARDLINK="06" SYMLINK="06" MISSING="00" ORPHAN="09" FIFO="06" SOCK="0B" OTHER="06"
-# export NNN_FCOLORS="$BLK$CHR$DIR$EXE$REG$HARDLINK$SYMLINK$MISSING$ORPHAN$FIFO$SOCK$OTHER"
-# export NNN_PLUG='n:nuke'
-# export NNN_OPENER=~/.config/nnn/plugins/nuke
