a53f7aded78361dfb06c290c17d91361f58c9b03
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-05-06 16:26:48 +0200

    Update .config/git/git_clone_bare_for_worktrees.sh

--- dot_config/git/executable_git_clone_bare_for_worktrees.sh
+++ dot_config/git/executable_git_clone_bare_for_worktrees.sh
@@ -13,19 +13,10 @@ basename=${url##*/}
 name=${2:-${basename%.*}}
 
 mkdir $name
-cd "$name"
 
-# Moves all the administrative git files (a.k.a $GIT_DIR) under .bare directory.
-#
-# Plan is to create worktrees as siblings of this directory.
-# Example targeted structure:
-# .bare
-# main
-# new-awesome-feature
-# hotfix-bug-12
-# ...
-git clone --bare "$url" .bare
-echo "gitdir: ./.bare" > .git
+git clone --bare "$url" "$name"
+
+cd "$name"
 
 # Explicitly sets the remote origin fetch so we can fetch remote branches
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
