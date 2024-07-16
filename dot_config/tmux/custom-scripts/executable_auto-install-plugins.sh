dddbc07a2f33d80c36e5589ca923d0deb05bbf0d
Laurent Hirth <laurent.hirth.68@icloud.com>
2024-07-16 14:24:42 +0200

    auto install tpm

--- dot_config/tmux/custom-scripts/executable_auto-install-plugins.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-tmux display -d 5000 'Installing TPM and plugins'
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
-~/.config/tmux/plugins/tpm/bin/install_plugins
